99004535f946fe9a2ce80d0b4dcb958c94a82c9c
ZhiFeng Hu <hufeng1987@gmail.com>
2015-11-04 15:00:59 +0800

    Try to listen and hosting port
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+)
 
 func main() {
-	http.ListenAndServe(":80", http.FileServer(http.Dir("./www")))
+	webdir := flag.String("d", "./www", "Host directory")
+	port := flag.Int("p", 80, "Listen port")
+	flag.Parse()
+	fmt.Println(fmt.Sprintf("Now hosting %s on port %d", *webdir, *port))
+	go func(port int, webdir string) {
+		err := http.ListenAndServe(fmt.Sprintf(":%d", port), http.FileServer(http.Dir(webdir)))
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(*port, *webdir)
+
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt)
+forever:
+	for {
+		select {
+		case <-sig:
+			fmt.Println("Signal recieved, now stop and exit")
+			break forever
+
+		}
+
+	}
 }
